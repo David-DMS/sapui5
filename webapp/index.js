28368e14f403d33d86c0231c4fd6499a4f2112cf
David Sturzbecher <david.sturzbecher@sap.com>
2022-10-13 23:28:07 -0300

    added a view to the project

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,16 @@
 sap.ui.define([
-    "sap/m/Button"
+    "sap/m/Button",
+    "sap/ui/core/mvc/XMLView"
 
-], function(Button) {
+], function(Button, XMLView) {
     "use strict";
     
+    XMLView.create({
+        viewName: "sap.ui.demo.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    })
+
     new Button({
         text: "Hello World!"
     }).placeAt("content");
