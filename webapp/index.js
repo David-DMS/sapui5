c5309ffb15f69d9957fc6fabb97fedf1dd85553d
David Sturzbecher <david.sturzbecher@sap.com>
2022-10-14 00:02:39 -0300

    added a controlller

--- webapp/index.js
+++ webapp/index.js
@@ -6,13 +6,9 @@ sap.ui.define([
     "use strict";
     
     XMLView.create({
-        viewName: "sap.ui.demo.App"
+        viewName: "sap.ui.demo.view.App"
     }).then(function (oView) {
         oView.placeAt("content");
     })
-
-    new Button({
-        text: "Hello World!"
-    }).placeAt("content");
 
 });
